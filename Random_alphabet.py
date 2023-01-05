3537edb8450dd03d437e75dec9525ff03e7f613f
VEDANSH-SHARMA <sharma.vedansh007@gmail.com>
2023-01-06 00:27:42 +0530

    Add files via upload

--- Random_alphabet.py
+++ Random_alphabet.py
@@ -1,9 +1,19 @@
 import random
-i=0
-j=''
-while i < 4:
-    randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
-    i+=1
-    j+=randomUpperLetter
-print(j)
+import pandas as pd
 
+def r_a():
+    i=0
+    j=''
+    while i < 4:
+        randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
+        i+=1
+        j+=randomUpperLetter
+    return j
+data_list = []
+columns_list = ['ID']
+k=10000
+for val in range(k):
+    data_list.append(r_a())
+
+df = pd.DataFrame(data_list,columns=columns_list)
+df.to_csv("inventory_id_test_data", mode='w', index = False)
