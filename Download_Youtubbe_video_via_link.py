0a1aba7b217420501ddfa2c4fbf07b05c2a86c58
VEDANSH-SHARMA <sharma.vedansh007@gmail.com>
2023-02-23 22:26:08 +0530

    Add files via upload
    youtube download

---
+++ Download_Youtubbe_video_via_link.py
@@ -0,0 +1,21 @@
+pip install pytube
+
+from pytube import YouTube
+import os
+
+s = input()
+
+def downloadYouTube(videourl, path):
+
+    yt = YouTube(videourl)
+    yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    if not os.path.exists(path):
+        os.makedirs(path)
+    yt.download(path)
+
+downloadYouTube(s, "../Downloads")
+
+
+
+
+
